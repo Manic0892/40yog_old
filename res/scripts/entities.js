39939e1f9c355c496df1c6596e03d7a72f84b0e5
Manic <basbalmanic@gmail.com>
2013-01-12 03:34:08 -0800

    drunk commit

--- res/scripts/entities.js
+++ res/scripts/entities.js
@@ -1,15 +1,22 @@
-function world() {
-	this.height;
-	this.width;
-	this.blocks = [];
-	this.createBlocks = new function(url, coords) {
-		this.blocks.push(new blockType(url));
-		this.blocks[length-1].coords = coords;
+function world(options) {
+	this.defaultOptions = {width: 500, height: 500, cellSize: 32};
+	if (options == null) {
+		options = this.defaultOptions;
 	}
+	this.height = options.height;
+	this.width = options.width;
+	this.cellSize = options.cellSize;
+	this.blocks = [1,1,1
+		       ];
+	this.blockURLs = [];
 }
 
-function level() {
-	this.world = new world();
+function level(options) {
+	if (options == null) {
+		options = {cellSize:32};
+	}
+	//this.world = new world();
+	this.cellSize = options.cellSize || 32;
 	this.events = [];
 }
 
--- res/scripts/main.js
+++ res/scripts/main.js
@@ -1,4 +1,7 @@
 window.onload = function() {
 	jaws.assets.add(['res/img/block.png', 'res/img/bullet.png', 'res/img/droid.png', 'res/img/droidarm.png']);
+	var newLevel =  new level({cellSize:22});
+	console.log(newLevel.events);
+	console.log(newLevel.cellSize);
 	//jaws.start(menuState);
 }
